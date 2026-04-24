b5fec43947083b17464d07b74ca7fa2d81160e2f
Marcio Turcios <mmttkav24@gmail.com>
2026-04-24 15:13:17 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,16 @@
-import { db } from "../../firebase";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAd5qoFZMVKV-hkIB6375kaRchoN763b3Y",
   authDomain: "homepath-12571.firebaseapp.com",
-  projectId: "homepath-12571"
+  projectId: "homepath-12571",
+  storageBucket: "homepath-12571.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
+const app = initializeApp(firebaseConfig);
+
+// ✅ THIS is your database
 export const db = getFirestore(app);
